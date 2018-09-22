c887269759a07d25bfabd05da1679b81bfde793a
Alex Fer <alexfera0@gmail.com>
2018-09-22 15:43:11 +0200

    add some css transitions and animations

--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -15,7 +15,7 @@ export class ProjectsService {
     const prjLoader = Observable.create((observer: Observer<Project[]>) => {
       setTimeout(() => {
         observer.next(this.projects);
-      }, 1200);
+      }, 2000);
     });
     return prjLoader;
   }
