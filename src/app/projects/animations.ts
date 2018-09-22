3ceb04049c2db7ea982b8e026010448f83756949
Alex Fer <alexfera0@gmail.com>
2018-09-22 20:58:24 +0200

    list animations

--- src/app/projects/animations.ts
+++ src/app/projects/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 export const markedTrigger = trigger( 'markedState', [
     state('default', style({
@@ -29,3 +29,44 @@ export const markedTrigger = trigger( 'markedState', [
     ])
 ]);
 
+export const itemStateTrigger = trigger('itemState', [
+    transition(':enter', [
+        style({
+            opacity: 0,
+            transform: 'translateX(-100%)'
+        }),
+        animate('500ms ease-out', keyframes([
+            style({
+                opacity: 0,
+                transform: 'translateX(-60%)',
+                offset: 0
+            }),
+            style({
+                opacity: 1,
+                transform: 'translateX(15%)',
+                offset: 0.3
+            }),
+            style({
+                opacity: 1,
+                transform: 'translateX(0%)',
+                offset: 1
+            }),
+        ]))
+    ]),
+    transition(':leave', [
+        animate('500ms ease-in', keyframes([
+            style({
+                opacity: 1,
+                transform: 'translateX(0%)'
+            }),
+            style({
+                opacity: 1,
+                transform: 'translateX(-5%)'
+            }),
+            style({
+                opacity: 1,
+                transform: 'translateX(100%)'
+            }),
+        ]))
+    ])
+]);
